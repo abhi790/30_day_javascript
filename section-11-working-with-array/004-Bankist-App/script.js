be3126282d050e19ee54bb5cceab6885553f95fa
abhi790 <abhimanyu.roy4000@gmail.com>
2026-02-06 18:48:50 +0530

    updated displayMovements function to work on sort button click event so that the movement are sorted in decending order top to bottom, and un-sort back to normal state when again clicked, implement text change from downarrow sort to uparrow unsort when toggled.

--- section-11-working-with-array/004-Bankist-App/script.js
+++ section-11-working-with-array/004-Bankist-App/script.js
@@ -68,9 +68,10 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = "";
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
     const html = `
         <div class="movements__row">
@@ -209,6 +210,18 @@ btnClose.addEventListener("click", function (event) {
     // Hide UI
     containerApp.style.opacity = 0;
   }
+});
+
+// event handler for the sort button
+// global state variable to preserve the value even after click sorting
+let sorted = false;
+btnSort.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  displayMovements(currentAccount.movements, !sorted);
+  // set toggle
+  sorted = !sorted;
+  btnSort.textContent = sorted ? "↑ Un-SORT" : "↓ SORT";
 });
 
 /////////////////////////////////////////////////

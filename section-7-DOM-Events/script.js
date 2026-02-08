81dd042219954c89aaa310f8fd9ced6fd7543353
abhi790 <abhimanyu.roy4000@gmail.com>
2026-02-08 14:09:53 +0530

    code refactoring done, so that our code looks clean and efficient

--- section-7-DOM-Events/script.js
+++ section-7-DOM-Events/script.js
@@ -7,47 +7,52 @@ const checkD = document.querySelector(".check");
 const guessD = document.querySelector(".guess");
 const highScoreD = document.querySelector(".highscore");
 const againD = document.querySelector(".again");
+const bodycolor = document.querySelector("body").style;
 // console.log(messageD.textContent);
 // console.log(number.textContent);
 
-let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let secretNumber = generateSecretNumber();
 // numberD.textContent = secretNumber;
 
 let score = 20;
 let highscore = 0;
+
+highScoreD.textContent = highscore;
+
 checkD.addEventListener("click", function (event) {
   const guessIn = Number(guessD.value);
   event.preventDefault();
   //   console.log(this); //return element on which event listener attached
   //   When there is No input
   if (!guessIn) {
-    messageD.textContent = "No number!";
+    displayMessage("No number!");
   }
   //   When Player wins
   else if (guessIn === secretNumber) {
     changeBackground(true); //to green background as we are passing with true argument
-    messageD.textContent = "Correct Number :-)";
-    highscore = highscore > score ? highscore : score;
-    highScoreD.textContent = `${highscore}`;
-    numberD.textContent = `${secretNumber}`;
+    displayMessage("Correct Number :-)");
+    numberD.textContent = secretNumber;
     numberD.style.width = "30rem";
-  }
-  //   When guess is too high - #222
-  else if (guessIn >= secretNumber) {
-    changeBackground(); //without argument will take color as #222
-    if (score > 1) {
-      guessHighMessage();
-    } else {
-      lostGameMessage();
+
+    if (score > highscore) {
+      highscore = score;
+      highScoreD.textContent = highscore;
     }
   }
-  //   When guess is too low - #222
-  else if (guessIn <= secretNumber) {
+  //   When guess is too high - #222
+  else if (guessIn !== secretNumber) {
     changeBackground(); //without argument will take color as #222
     if (score > 1) {
-      guessLowMessage(score);
+      displayMessage(
+        guessIn > secretNumber ? "Too High - UpArrow" : "Too Low  - DownArrow",
+      );
+      score--;
+      scoreD.textContent = score;
+
+      numberD.textContent = "?";
     } else {
-      lostGameMessage();
+      displayMessage("You lost the game");
+      scoreD.textContent = "0";
     }
   }
 });
@@ -55,40 +60,28 @@ checkD.addEventListener("click", function (event) {
 // attaching click event handler
 againD.addEventListener("click", function (event) {
   event.preventDefault();
-  secretNumber = Math.trunc(Math.random() * 20 + 1);
-  messageD.textContent = "Start guessing...";
+  secretNumber = generateSecretNumber();
+  changeBackground();
+  displayMessage("Start guessing...");
   guessD.value = "";
   highscore = score;
   score = 20;
   numberD.textContent = "?";
   numberD.style.width = "15rem";
-  scoreD.textContent = `${score}`;
-  changeBackground();
+  scoreD.textContent = score;
 });
 
 // utility function
 // changing background color based on value passed and default value
 function changeBackground(win = false) {
   if (win) {
-    document.querySelector("body").style.backgroundColor = "#60b347";
-  } else document.querySelector("body").style.backgroundColor = "#222";
+    bodycolor.backgroundColor = "#60b347";
+  } else bodycolor.backgroundColor = "#222";
+}
+function displayMessage(message) {
+  messageD.textContent = message;
 }
 
-// lost game message
-function lostGameMessage() {
-  messageD.textContent = "You lost the game";
-  scoreD.textContent = "0";
-}
-
-// guess too low message
-function guessLowMessage(score) {
-  messageD.textContent = "Too Low  - DownArrow";
-  score--;
-  scoreD.textContent = `${score}`;
-}
-
-function guessHighMessage(score) {
-  messageD.textContent = "Too High - UpArrow";
-  score--;
-  scoreD.textContent = `${score}`;
+function generateSecretNumber() {
+  return Math.trunc(Math.random() * 20 + 1);
 }

4dbae334eec65a2d56c0d0492330cec1a55878ac
abhi790 <abhimanyu.roy4000@gmail.com>
2026-02-08 12:01:40 +0530

    we changed the logic of the game, when we win the guess body color is changed to green when we make wrong guess it changes back to initial background color, we have done this using javascript that add inline style to the class specified in the code

--- section-7-DOM-Events/script.js
+++ section-7-DOM-Events/script.js
@@ -1,29 +1,58 @@
-// document.querySelector('.message')
-// document.querySelector('.number')
-// document.querySelector('.score')
-// document.querySelector(".check");
-// document.querySelector('.guess')
-
 "use strict";
 // D means DOM
 const messageD = document.querySelector(".message");
 const numberD = document.querySelector(".number");
-const scoreD = document.querySelector(".check");
+const scoreD = document.querySelector(".score");
 const checkD = document.querySelector(".check");
 const guessD = document.querySelector(".guess");
+const highScoreD = document.querySelector(".highscore");
 // console.log(messageD.textContent);
 // console.log(number.textContent);
 
-messageD.textContent = "Correct Number!";
 const secretNumber = Math.trunc(Math.random() * 20 + 1);
-numberD.textContent = secretNumber;
+// numberD.textContent = secretNumber;
 
 let score = 20;
-
+let highscore = 0;
 checkD.addEventListener("click", function (event) {
   const guessIn = Number(guessD.value);
+  event.preventDefault();
   //   console.log(this); //return element on which event listener attached
+  //   When there is No input
   if (!guessIn) {
     messageD.textContent = "No number!";
+  }
+  //   When Player wins
+  else if (guessIn === secretNumber) {
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    messageD.textContent = "Correct Number :-)";
+    highscore = highscore > score ? highscore : score;
+    highScoreD.textContent = `${highscore}`;
+    numberD.textContent = `${secretNumber}`;
+    numberD.style.width = "30rem";
+  }
+  //   When guess is too high - #222
+  else if (guessIn >= secretNumber) {
+    document.querySelector("body").style.backgroundColor = "#222";
+    if (score > 1) {
+      messageD.textContent = "Too High - UpArrow";
+      score--;
+      scoreD.textContent = `${score}`;
+    } else {
+      messageD.textContent = "You lost the game";
+      scoreD.textContent = "0";
+    }
+  }
+  //   When guess is too low - #222
+  else if (guessIn <= secretNumber) {
+    document.querySelector("body").style.backgroundColor = "#222";
+    if (score > 1) {
+      messageD.textContent = "Too Low  - DownArrow";
+      score--;
+      scoreD.textContent = `${score}`;
+    } else {
+      messageD.textContent = "You lost the game";
+      scoreD.textContent = "0";
+    }
   }
 });

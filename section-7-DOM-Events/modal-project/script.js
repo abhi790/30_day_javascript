8c0da579110f1e3e624993e7110a097704180bc9
abhi790 <abhimanyu.roy4000@gmail.com>
2026-02-08 14:36:51 +0530

    we have used querySelector and querySelectorAll to tap our ODOM element so that we can manipulate them in the next chapter

--- section-7-DOM-Events/modal-project/script.js
+++ section-7-DOM-Events/modal-project/script.js
@@ -1,1 +1,14 @@
 "use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+// console.log(modal);
+// console.log(overlay);
+// console.log(btnCloseModal);
+console.log(btnOpenModal);
+
+for (const element of btnOpenModal) {
+  console.log(element.textContent);
+}

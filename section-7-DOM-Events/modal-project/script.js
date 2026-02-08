4a5b8ea456dfc664d032b0d5fd126c1cc3fec218
abhi790 <abhimanyu.roy4000@gmail.com>
2026-02-08 15:01:52 +0530

    created modal opening and closing functionality using addEventListener and callback handler function, we practice dry don't repeat yourself for better maintainability of the codebase, we used for of loop to attach event listener to all the three button having same class, we used querySelectorAll to get array of button nodelist so that we can loop over it and attach eventlistener to each and every button, the app is working fine

--- section-7-DOM-Events/modal-project/script.js
+++ section-7-DOM-Events/modal-project/script.js
@@ -4,11 +4,20 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnOpenModal = document.querySelectorAll(".show-modal");
 
-// console.log(modal);
-// console.log(overlay);
-// console.log(btnCloseModal);
-console.log(btnOpenModal);
+const closeModalWinHandler = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+const openModalWinHandler = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
 for (const element of btnOpenModal) {
-  console.log(element.textContent);
+  element.addEventListener("click", openModalWinHandler);
 }
+// when we click button at top right corner of modal window - it should close modal
+btnCloseModal.addEventListener("click", closeModalWinHandler);
+
+// when we click outside of modal window it should also close modal
+overlay.addEventListener("click", closeModalWinHandler);

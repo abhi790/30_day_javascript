eaee01ac0dd417b5ac02c3627d8dd631320bf01e
abhi790 <abhimanyu.roy4000@gmail.com>
2026-02-09 17:38:33 +0530

    we just clear our script file and move the code of eventlistener to 003-type-eventlistener.html

--- section-13-Advance-DOM/BankingApp/script.js
+++ section-13-Advance-DOM/BankingApp/script.js
@@ -32,31 +32,3 @@ document.addEventListener("keydown", function (e) {
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
-
-// We can SET  event listener using 3 different way
-// 1. using .addEventListener() - preferred way to use event listener
-// 2. .onclick(handlerfunction), .onmouseenter(handlerfuntion) like this - old way not recommended
-// 3. using html attribut <h1 onclick="do what you want or use a handler function"></h1>
-
-// 1. USING .addEventListener() - we will use this only and rest for only good to know
-const handlerFunction = (e) => {
-  console.log(`You have pressed key : ${e.key}`);
-};
-// when we press any key on keyboard below event handler will fire
-document.addEventListener("keydown", handlerFunction);
-// we can remove event handler as well
-document.removeEventListener("keydown", handlerFunction);
-
-// 2. USING .onclick
-const h1 = document.querySelector("h1");
-const mouseHandlerFunction = (e) => {
-  console.log(`You have hover the h1 element`);
-};
-h1.onmouseenter = mouseHandlerFunction;
-
-// 3. Way is manipulating html element itself
-// let's create an element and attached it to any sections and then add event listener to that element
-const element = document.createElement("h1");
-element.innerHTML = `<h1 onclick="alert('You have click newly created h1 element')">This h1 is inserted using javascript!</h1>`;
-
-modal.insertAdjacentElement("beforeend", element);

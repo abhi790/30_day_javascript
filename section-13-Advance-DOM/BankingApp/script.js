d9fc193416fb6c000cd319a1c04abca649753411
abhi790 <abhimanyu.roy4000@gmail.com>
2026-02-12 09:07:35 +0530

    we learned how to traverse DOM tree going downward and going upward in the tree using h1 as reference. we also find all the sibling element by going upward in the tree to parent and listing its child that's the trick we used to get all the sibling of h1

--- section-13-Advance-DOM/BankingApp/script.js
+++ section-13-Advance-DOM/BankingApp/script.js
@@ -32,3 +32,35 @@ document.addEventListener("keydown", function (e) {
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
 ////////////////////////////////////////////////////
+
+// TRAVERSING DOM TREE
+const h1 = document.querySelector("h1");
+
+// GOING DOWNWARD : CHILD
+console.log(h1.querySelectorAll(".highlight")); //it will show every highlight class inside h1 not just direct children
+console.log(h1.childNodes);
+console.log(h1.children);
+// we can modify or set element property
+h1.firstElementChild.style.color = "white";
+h1.lastElementChild.style.color = "blue";
+
+// GOING UPWARD : PARENTS
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+console.log(h1.closest(".header"));
+
+h1.closest(".header").style.background = "var(--gradient-secondary)";
+h1.closest("h1").style.background = "var(--gradient-primary)";
+
+// GOING SIDEWAYS : Siblings elements
+console.log(h1.previousElementSibling); //null - as h1 is the first child of the div.header__title
+console.log(h1.nextElementSibling);
+
+// getting all the sibling of h1 - trick, first go to parent and then list all the children
+console.log(h1.parentElement.children);
+
+// do some fun things
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = "scale(0.5)";
+});
